75f2c460560bf086ac3a7a851169430b14826463
Hoang Nguyen <hoang.viet.nguyen@outlook.com>
2020-01-13 20:51:14 -0500

    added jwt auth for backend

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');
+const config = require('config');
 
 //require('dotenv').config();
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 
-const db = `mongodb+srv://Hoang:sPP4Vxtv51Uwa3Yk@cluster0-wj4ek.mongodb.net/test?retryWrites=true&w=majority`;
+const db = config.get('mongoURI');
 
 mongoose
-  .connect(db)
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log('Connected To MongoDB'))
   .catch(err => console.log(err));
 
 //Use Routes
 
 app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 //Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
--- routes/api/items.js
+++ routes/api/items.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
 //Item model
 const Item = require('../../models/Item');
@@ -15,16 +16,16 @@ router.get('/', (req, res) => {
 
 // @route POST api/items
 // @desc Create a item
-// @access Public
-router.post('/', (req, res) => {
+// @access Private
+router.post('/', auth, (req, res) => {
   const newItem = new Item({ name: req.body.name });
   newItem.save().then(item => res.json(item));
 });
 
 // @route DELETE api/items
 // @desc Delete an item
-// @access Public
-router.delete('/:id', (req, res) => {
+// @access Private
+router.delete('/:id', auth, (req, res) => {
   Item.findById(req.params.id)
     .then(item => item.remove().then(() => res.json({ success: true })))
     .catch(err => res.status(404).json({ success: false }));
